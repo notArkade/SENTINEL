6ed5c583d1331d56bef7ecbc21a582301f86be7e
Arkadip <arkadip671@gmail.com>
2025-03-23 02:29:30 +0530

    fixed footer

--- frontend/src/sections/Footer.jsx
+++ frontend/src/sections/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <Element name="about">
       <footer className="backdrop-blur-md text-white text-center p-10 py-32">
-        <div className="grid grid-cols-3 items-center justify-center space-x-4">
+        <div className="grid grid-cols-3 items-center justify-center">
           <p className="text-xl font-extrabold uppercase bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text tracking-[0.4em]">
             Sentinel
           </p>
