f66047936fbdced2c9504a6a356e5422b4454f3a
Arkadip <arkadip671@gmail.com>
2025-03-23 02:10:44 +0530

    added footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,17 @@ import Form from "./sections/Form";
 import Rings from "./components/Rings";
 import Hero from "./sections/Hero";
 import Navbar from "./components/Navbar";
+import Footer from "./sections/Footer";
 
 function App() {
   return (
     <main className="scroll-hide">
-    <div className="min-h-screen bg-black items-center justify-center">
-      <Navbar />
-      <Hero />
-      <Form />
-    </div>
+      <div className="min-h-screen bg-black items-center justify-center">
+        <Navbar />
+        <Hero />
+        <Form />
+        <Footer />
+      </div>
     </main>
   );
 }
---
+++ frontend/src/sections/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Element } from "react-scroll";
+
+const Footer = () => {
+  return (
+    <Element name="about">
+      <footer className="backdrop-blur-md text-white text-center p-10 py-32">
+        <div className="grid grid-cols-3 items-center justify-center space-x-4">
+          <p className="text-xl font-extrabold uppercase bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text tracking-[0.4em]">
+            Sentinel
+          </p>
+
+          <p>By Arkadip Das</p>
+          <p>Arkade &copy; 2025</p>
+        </div>
+      </footer>
+    </Element>
+  );
+};
+
+export default Footer;
